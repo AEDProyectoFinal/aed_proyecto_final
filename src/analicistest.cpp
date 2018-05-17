f45054426ec0e51140fa9ab8297d8f01466069c1
ansesame <33558005+ansesame@users.noreply.github.com>
2018-05-16 19:28:07 -0500

    Se unificó la lectura y la creación del mapa.

--- src/analicistest.cpp
+++ src/analicistest.cpp
@@ -12,6 +12,8 @@
 #include<map>
 using namespace std;
 
+
+//Función para imprimir el histograma
 void imprimirmapa(map <string, int> &tabela) {
   map<string, int>::iterator it;
   for (it = tabela.begin(); it != tabela.end(); ++it) {
@@ -20,105 +22,97 @@ void imprimirmapa(map <string, int> &tabela) {
 }
 
 
-int main(){
-
-	ArbolesAVL<string> lexicon;
-	map<string, int> palanali;
-
-//Creación del lexicon de stopwords: Aquí se crea un arbol binario balanceado para comparar con mayor agilidad las palabras.
+//Creación del arbol con el lexicón
+void crearlexicon(ArbolesAVL<string> & arbol){
 	ifstream stopwords;
 	string stopw;
 	stopwords.open("data/stopwords-es.txt");
 	while(getline(stopwords,stopw)){
-		lexicon.insertar(stopw);
+		arbol.insertar(stopw);
 	}
 	stopwords.close();
-
-	lexicon.imprimir();
-
-
-//Lectura de la información
-	ifstream datos;
-	string linea;
-	datos.open("data/prueba.txt");
-	while(getline(datos, linea)){
-		string palabra = "";
-		for(int i = 0; i < linea.length(); i++ ){
-			if(linea[i]==' '||linea[i]==','||linea[i]==';'||linea[i]==':'||linea[i]=='('||linea[i]==')'||linea[i]=='.'){
-				if(!palabra.empty()){    //Se comprueba que no esté vacia la palabra.
-					if(!lexicon.buscar(palabra)){    //Se comprueba que no esté en el lexicón de stopwords
-						if(palanali.find(palabra) == palanali.end()){
-							palanali.insert(pair<string,int>(palabra,0));
-						}else{
-							palanali[palabra]++;
-						}
-					}
-				}
-				palabra = "";
-			}else{
-				palabra+= linea[i];
-			}
-		}
-		//Nuevamente para incluir la ultima palabra
-		if(!palabra.empty()){    //Se comprueba que no esté vacia la palabra.
-			if(!lexicon.buscar(palabra)){    //Se comprueba que no esté en el lexicón de stopwords
-				if(palanali.find(palabra) == palanali.end()){
-					palanali.insert(pair<string,int>(palabra,0));
-				}else{
-					palanali[palabra]++;
-				}
-			}
-		}
-	}
-	datos.close();
-	
-	imprimirmapa(palanali);
-
-
 }
 
 
 
-//Nueva parte pa combinar introduciendo la sección de analicis de las palabras e introducirlas al histograma.
-
-#include<iostream>
-#include<fstream>
-#include<string>
-using namespace std;
 
 int main(){
+
+	ArbolesAVL<string> lexicon; //Arbol para stopwords
+	map<string, int> palanali;
+
+
+	crearlexicon(lexicon);//Se lena el arbol de stopwords
+	lexicon.imprimir();
+
+
+
 	ifstream doc;
-	doc.open("data/Twitter_API.txt");
-	string linea;
-	string tweet= "";
-	while(getline(doc, linea)){
-		for(int i = 0; i<linea.length(); i++){
-			if(linea[i]=='"'&&linea[i+1]=='t'&&linea[i+2]=='e'&&linea[i+3]=='x'&&linea[i+4]=='t'){
-				int e = i+8;
-				while(true){
-					if(linea[e]=='"')break;
-					else{
-					tweet.push_back(linea[e]);
-					e++;
+		doc.open("data/Twitter_API.txt");
+		string linea;
+		string tweet= "";
+		while(getline(doc, linea)){
+			for(int i = 0; i<linea.length(); i++){
+				if(linea[i]=='"'&&linea[i+1]=='t'&&linea[i+2]=='e'&&linea[i+3]=='x'&&linea[i+4]=='t'){
+					int e = i+8;
+					while(true){
+						if(linea[e]=='"')break;
+						else{
+						tweet.push_back(linea[e]);
+						e++;
+						}
+
 					}
+					if(!(tweet[0]=='R'&&tweet[1]=='T'&&tweet[2]==' ')){
+						cout << tweet << endl;//Remover posteriormente
+						//En esta linea se puede guardar el tweet en una estructura.
 
-				}
-				if(!(tweet[0]=='R'&&tweet[1]=='T'&&tweet[2]==' ')){
-					cout << tweet << endl;
-					//En esta linea se puede guardar el tweet en una estructura.
+						//Contrucción del histograma.
+						string palabra = "";
+						for(int it2 = 0; it2 < tweet.length(); it2++ ){
+							if(tweet[it2]==' '||tweet[it2]==','||tweet[it2]==';'||tweet[it2]==':'||tweet[it2]=='('||tweet[it2]==')'||tweet[it2]=='.'){
+								if(!palabra.empty()){    //Se comprueba que no esté vacia la palabra.
+									if(!lexicon.buscar(palabra)){    //Se comprueba que no esté en el lexicón de stopwords
+										if(palanali.find(palabra) == palanali.end()){
+										    palanali.insert(pair<string,int>(palabra,1));
+										}else{
+											palanali[palabra]++;
+										}
+										cout << palabra << "---";//Remover posteriormente
+									}
+								}
+								palabra = "";
+							}else{
+								palabra+= tweet[it2]; //Se van agregando los caracteres para formar la palabra
+							}
+						}
+						//Nuevamente para incluir la ultima palabra
+						if(!palabra.empty()){    //Se comprueba que no esté vacia la palabra.
+							if(!lexicon.buscar(palabra)){    //Se comprueba que no esté en el lexicón de stopwords
+								if(palanali.find(palabra) == palanali.end()){
+									palanali.insert(pair<string,int>(palabra,1));
+								}else{
+									palanali[palabra]++;
+								}
+								cout << palabra << "---";//Remover posteriormente
+							}
+						}
 
+					}
+					i = i + tweet.size()+7;
+					tweet = "";
 				}
-				i = i + tweet.size()+7;
-				tweet = "";
 			}
 		}
-	}
 	doc.close();
 
 
 
-}
+	imprimirmapa(palanali);
 
+
+
+}
 
 
 

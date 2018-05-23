42f2ca634ef78c4c5e37795fb8ccd97211b32d6e
juanmigutierrez <33558019+juanmigutierrez@users.noreply.github.com>
2018-05-23 18:07:12 -0500

    Update llenar_full.cpp
    Se agrego la funciont txt_mapa

--- src/llenar_full.cpp
+++ src/llenar_full.cpp
@@ -1,10 +1,3 @@
-/*
- * llenar_full.cpp
- *
- *  Created on: 23/05/2018
- *      Author: vonnewmann
- */
-
 #include <iostream>
 #include <map>
 #include "ArbolAVL.cpp"
@@ -27,31 +20,95 @@ void guardar( map<string, int> & mapa,string palabra){
 							cout << "no se pudo abrir"<< endl;				}
 	}
 	else if(palabra == "duque"){
-		archivo.open("duque.txt",std::ios_base::app);
+		archivo.open("duque.txt");
+		if(archivo.is_open()){
 				for(it = mapa.begin();it!=mapa.end();it++){
 					archivo << it->first << "," <<it->second ;
+					archivo.flush();
 					}
 		}
+		else{
+			cout << "no se pudo abrir"<< endl;
+		}
+		}
 	else{
-		archivo.open("fajardo.txt",std::ios_base::app);
+		archivo.open("fajardo.txt");
+		if(archivo.is_open()){{
 				for(it = mapa.begin();it!=mapa.end();it++){
 				archivo << it->first << "," <<it->second ;
+				archivo.flush();
 	}
-				archivo.close();
+		}
 
+		}
 
 }
+	archivo.close();
+}
+
+void txt_mapa(map<string, int> & mapa,string palabra){
+	std::map<string, int>::iterator it;
+	std::ifstream archivo;
+	string linea;
+	string delimitador =",";
+	if(palabra == "petro"){
+		archivo.open("petro.txt");
+		if(archivo.is_open()){
+				cout << "lo pudo abrir para leer" << endl;
+	}
+		else {
+				cout << "no se pudo abrir"<< endl;
+		}
+	}
+	else if(palabra == "duque"){
+		archivo.open("duque.txt");
+		if(archivo.is_open()){
+			cout << "lo pudo abrir para leer" << endl;
+		}
+		else{
+			cout << "no se pudo abrir"<< endl;
+		}
+		}
+	else{
+		archivo.open("fajardo.txt");
+		if(archivo.is_open()){
+			cout << "lo pudo abrir para leer" << endl;
+		}
+		else{
+			cout << "no se pudo abrir"<< endl;
+
+		}
+
+}
+	while(getline(archivo,linea)){
+		string palabra = linea.substr(0,linea.find(delimitador));
+		string entero = linea.substr(linea.find(delimitador)+1, linea.length());
+		int entero1 = std::stoi(entero);
+		mapa.insert(pair<string,int>(palabra,entero1));
+	}
+	archivo.close();
+}
+
+void imprimir(map<string, int> &mapa){
+	std::map<string, int>::iterator it;
+	for(it = mapa.begin();it!=mapa.end();it++){
+						cout << it->first << "//" << it->second<<endl;
+					}
 }
 
 int main(){
 	map<string, int> map;
+	std::map<string, int> mapa;
 	map.insert(pair<string,int>("jose",1));
 	map.insert(pair<string,int>("mike",1));
 	map.insert(pair<string,int>("alo",1));
 	map.insert(pair<string,int>("jquin",1));
 	string petro = "petro";
 	guardar(map,petro);
+	txt_mapa(mapa,petro);
+	imprimir(mapa);
 	return 0;
 }
+
 
 
